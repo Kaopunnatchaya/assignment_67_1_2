eee4e97f348ea69c9e3f396e45c062cf1b0f93d9
Kaopunnatchaya <boonnak9871@gmail.com>
2025-09-13 13:23:07 +0700

    step 6 : Firestore CRUB oparetion

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // ต้องมีไฟล์นี้
+import 'firebase_options.dart'; 
 import 'anime_list_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform, // ✅ ต้องมี
+    options: DefaultFirebaseOptions.currentPlatform, 
   );
   runApp(const MyApp());
 }
